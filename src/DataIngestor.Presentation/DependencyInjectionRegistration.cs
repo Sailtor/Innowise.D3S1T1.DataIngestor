3f94bb303628413d441d196daa0f5384aced5d42
Sailtor <Klimentenko02@mail.ru>
2025-12-07 23:25:11 +0300

    Initial commit Added job class Injected quartz config and services to DI

--- src/DataIngestor.Presentation/DependencyInjectionRegistration.cs
+++ src/DataIngestor.Presentation/DependencyInjectionRegistration.cs
@@ -1,5 +1,12 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using DataIngestor.Presentation.Jobs;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Quartz;
+using Quartz.Impl.Calendar;
+using Quartz.Impl.Matchers;
+using System.Diagnostics.Metrics;
+using System.Globalization;
+using System.Security.Principal;
 
 namespace DataIngestor.Presentation;
 
@@ -8,10 +15,41 @@ public static class DependencyInjectionRegistration
     public static void AddPresentationServices(this IServiceCollection services, IConfiguration configuration)
     {
         AddControllers(services);
+        AddQuartzJobs(services, configuration);
     }
 
     private static void AddControllers(IServiceCollection services)
     {
         services.AddControllers();
+    }
+
+    private static void AddQuartzJobs(IServiceCollection services, IConfiguration configuration)
+    {
+        services.Configure<QuartzOptions>(configuration.GetSection("Quartz"));
+        services.Configure<QuartzOptions>(options =>
+        {
+            options.Scheduling.IgnoreDuplicates = true;
+            options.Scheduling.OverWriteExistingData = false;
+        });
+
+        services.AddQuartz(q =>
+        {
+            // handy when part of cluster or you want to otherwise identify multiple schedulers
+            q.SchedulerId = "Scheduler-Core";
+            q.UseSimpleTypeLoader();
+            q.UseInMemoryStore();
+            q.UseDefaultThreadPool(tp =>
+            {
+                tp.MaxConcurrency = 10;
+            });
+
+            // quickest way to create a job with single trigger is to use ScheduleJob
+            q.ScheduleJob<MetricReaderJob>(trigger => trigger
+                .WithIdentity("10 second cron Trigger")
+                .StartNow()
+                .WithCronSchedule("0/3 * * * * ?")
+                .WithDescription("10 second cron trigger")
+            );
+        });
     }
 }
---
+++ src/DataIngestor.Presentation/Jobs/MetricReaderJob.cs
@@ -0,0 +1,20 @@
+﻿using DataIngestor.Application.Interfaces;
+using Microsoft.Extensions.Logging;
+using Quartz;
+
+namespace DataIngestor.Presentation.Jobs;
+
+public class MetricReaderJob(
+    IMetricReader metricReader,
+    ILogger<MetricReaderJob> logger) : IJob
+{
+    private readonly IMetricReader metricReader = metricReader;
+    private readonly ILogger<MetricReaderJob> logger = logger;
+
+    public async Task Execute(IJobExecutionContext context)
+    {
+        logger.LogInformation($"Metrics fetch started at {DateTime.UtcNow}");
+        var metrics = await metricReader.ReadMetricsAsync();
+        logger.LogInformation(metrics.ToString()); // To test metrics reading. Will be switched to MQ message posting
+    }
+}
